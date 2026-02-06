223dc954859247ddae3268fc06a12ed43cedbaf4
tentumoshi <wakinsarabia@gmail.com>
2026-02-06 14:24:58 +0800

    add comment test

---
+++ index.js
@@ -0,0 +1,34 @@
+const toggleLightOrDarkBtn = document.getElementById('toggleLightDark');
+const titleElement = document.querySelector('.title');
+let currentTheme = localStorage.getItem('theme') || 'light';
+
+
+// alert('Light mode activated');
+if (currentTheme === 'dark') {
+    document.body.style.backgroundColor = '#1a1a1a';
+    toggleLightOrDarkBtn.textContent = 'Light';
+} else {
+    document.body.style.backgroundColor = '#f0f0f0';
+    toggleLightOrDarkBtn.textContent = 'Dark';
+}
+
+toggleLightOrDarkBtn.addEventListener('click', () => {
+    if (currentTheme === 'light') {
+        // titleElement.style.color = 'white';
+        toggleLightOrDarkBtn.textContent = 'Light';
+        document.body.style.backgroundColor = '#1a1a1a';
+        currentTheme = 'dark';
+        // alert('Dark mode activated');
+    } else {
+        // titleElement.style.color = 'black'; asfdsd
+        toggleLightOrDarkBtn.textContent = 'Dark';
+        document.body.style.backgroundColor = '#f0f0f0';
+        currentTheme = 'light';
+        // alert('Light mode activated');
+    }
+
+
+    
+
+    localStorage.setItem('theme', currentTheme);
+});
