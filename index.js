f436046f50154d4f934966d358c30a3f8e60fb4f
tentumoshi <wakinsarabia@gmail.com>
2026-06-22 21:53:25 +0800

    Update project card image and styles; replace image source and adjust background color for improved aesthetics

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const sunIcon = document.querySelector('.darkMode-svg');
 const moonIcon = document.querySelector('.lightMode-svg');
 const progressBar = document.querySelectorAll('circle');
+
+const reactjs_card_bg = document.querySelector('.project-card.reactjs-card img');
+
 const text = document.querySelectorAll('span, p, h2, h3, h4, h5, h6');
+
+
 
 let currentTheme = localStorage.getItem('theme') || 'light';
 
@@ -9,8 +14,6 @@ function applyTheme(theme) {
     if (theme === 'dark') {
         document.documentElement.style.setProperty('--color-bg', '#141A20');
         document.documentElement.style.setProperty('--color-text', '#D0D1D3');
-        document.documentElement.style.setProperty('--color-primary', '#2C576A');
-        document.documentElement.style.setProperty('--color-dark-primary', '#07354C');
         sunIcon.style.display = 'none';
         moonIcon.style.display = 'inline';
         progressBar.forEach(circle => circle.style.stroke = '--color-dark-primary');
@@ -18,11 +21,12 @@ function applyTheme(theme) {
     } else {
         document.documentElement.style.setProperty('--color-bg', '#D0D1D3');
         document.documentElement.style.setProperty('--color-text', '#141A20');
-        document.documentElement.style.setProperty('--color-primary', '#2C576A');
-        document.documentElement.style.setProperty('--color-dark-primary', '#07354C');
         sunIcon.style.display = 'inline';
         moonIcon.style.display = 'none';
         progressBar.forEach(circle => circle.style.stroke = '--color-primary');
+
+        reactjs_card_bg.style.backgroundColor = '#141A20';
+
         text.forEach(p => p.style.color = 'black');
     }
 }
